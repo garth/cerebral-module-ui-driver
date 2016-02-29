10b9678c4956e8e677b0874a36b020dd69a6a01f
Garth Williams <garth@wi.llia.ms>
2016-02-29 06:53:20 -0800

    Fixed bug in date formatter

--- src/formatters/date.js
+++ src/formatters/date.js
@@ -1,5 +1,5 @@
 import moment from 'moment'
 
 export default function dateFormat (value, { dateFormat } = {}) {
-  return value === null || value === undefined ? '' : moment(value).format(dateFormat)
+  return value === null || value === undefined ? '' : moment(value).utcOffset(0).format(dateFormat)
 }
